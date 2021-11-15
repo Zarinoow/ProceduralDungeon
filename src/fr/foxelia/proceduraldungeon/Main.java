f819d45080deb9de5f563f7ef27134e8ee039528
Zarinoow <petits.minous12@gmail.com>
2021-11-15 09:50:50 +0100

    Base of the project

---
+++ src/fr/foxelia/proceduraldungeon/Main.java
@@ -0,0 +1,22 @@
+package fr.foxelia.proceduraldungeon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	/*
+	 * Todo list
+	 * 
+	 * 
+	 */
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
